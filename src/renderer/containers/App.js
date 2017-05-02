62318f4b8e7dd2476fabb3a3b92382ddbf34b10a
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-05-02 05:51:38 -0500

    wire up to the store with connect and add simple props validation

--- src/renderer/containers/App.js
+++ src/renderer/containers/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import MenuBar from './MenuBar';
 import DevTools from './DevTools';
 
 class App extends Component {
   render() {
+    const { hashes } = this.props;
     return (
       <div>
         <MenuBar />
+        <ul>
+          { hashes.map(h => <li key={ h }>{ h }</li>) }
+        </ul>
         <DevTools />
       </div>
     );
   }
 }
+App.propTypes = {
+  hashes: PropTypes.arrayOf(PropTypes.string),
+};
 
-export default App;
+export default connect(mapStateToProps)(App);
+
+function mapStateToProps(state) {
+  return {
+    hashes: Object.keys(state.previews),
+  };
+}
