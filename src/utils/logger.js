41f388a5bc89698246bfda8ffce6ded1c915e23c
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-05-09 05:55:29 -0500

    log ffmpeg operations

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,21 @@
-function logger(...args) {
-  console.log(...args); // eslint-disable-line no-console
-}
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import path from 'path';
+import { app } from 'electron';
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: path.join(app.getPath('appData'), 'log'),
+  datePattern: 'yyyy-MM-dd.',
+  prepend: true,
+  timestamp: true,
+  level: process.env.ENV === 'development' ? 'debug' : 'info', // eslint-disable-line no-process-env
+});
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'silly', timestamp: true, colorize: true }),
+    transport,
+  ],
+});
 
 export default logger;
