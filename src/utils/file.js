3b72c2ea5082fceb5e92900276de136eeab14587
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-05-12 20:39:43 -0500

    add logging for file hashing

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,13 +1,19 @@
+import logger from './logger';
 import crypto from 'crypto';
 import fs from 'fs';
 
 function hashFile(filePath) {
+  const start = Date.now();
   const hash = crypto.createHash('sha1');
   const stream = fs.createReadStream(filePath);
   return new Promise((resolve, reject) => {
     stream.on('error', err => reject(err));
     stream.on('data', data => hash.update(data, 'utf8'));
-    stream.on('end', () => resolve(hash.digest('hex')));
+    stream.on('end', () => {
+      const computedHash = hash.digest('hex');
+      logger.log('silly', `Completed hash for ${filePath} (${computedHash}) ${Date.now() - start}ms`);
+      resolve(computedHash);
+    });
   });
 }
 
