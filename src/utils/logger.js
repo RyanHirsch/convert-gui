b50c0e519bd8bb8260e796c681a7d2a7cdb687cb
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-05-12 21:28:20 -0500

    update logger to supress logs during testing

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,20 +2,26 @@ import winston from 'winston';
 import 'winston-daily-rotate-file';
 import path from 'path';
 import { app } from 'electron';
+import { isTest, isDevelopment } from './env';
 
-const transport = new winston.transports.DailyRotateFile({
-  filename: path.join(app.getPath('appData'), 'log'),
-  datePattern: 'yyyy-MM-dd.',
-  prepend: true,
-  timestamp: true,
-  level: process.env.ENV === 'development' ? 'debug' : 'info', // eslint-disable-line no-process-env
-});
+function getTransports() {
+  if(isTest()) {
+    return [];
+  }
+  return [
+    new (winston.transports.Console)({ level: 'silly', timestamp: true, colorize: true }),
+    new winston.transports.DailyRotateFile({
+      filename: path.join(app.getPath('appData'), 'log'),
+      datePattern: 'yyyy-MM-dd.',
+      prepend: true,
+      timestamp: true,
+      level: isDevelopment() ? 'debug' : 'info', // eslint-disable-line no-process-env
+    }),
+  ];
+}
 
 const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({ level: 'silly', timestamp: true, colorize: true }),
-    transport,
-  ],
+  transports: getTransports(),
 });
 
 export default logger;
